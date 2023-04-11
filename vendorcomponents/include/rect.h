1e90ddfe404075ca24cf146b0a8d184f180550f5
peng.wu <peng.wu@amlogic.com>
2023-04-11 13:58:11 +0800

    codec2: CF1 License&Copyright to Apache - HW-VDEC. [1/1]
    PD#SWPL-110606
    
    Problem:
    Codec2 License&Copyright in source code should be aligned - HW-VDEC.
    
    Solution:
    Licensed under the Apache License, Version 2.0 (the "License")
    
    Verify:
    OHM
    
    Change-Id: Icfa143d3857ba87667abb9dc9438308b512ea364
    Signed-off-by: peng.wu <peng.wu@amlogic.com>

--- vendorcomponents/include/rect.h
+++ vendorcomponents/include/rect.h
@@ -1,15 +1,18 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-// Note: ported from Chromium commit head: 0e161fe
-// Note: only necessary functions are ported from gfx::Rect
-
-// Defines a simple integer rectangle class.  The containment semantics
-// are array-like; that is, the coordinate (x, y) is considered to be
-// contained by the rectangle, but the coordinate (x + width, y) is not.
-// The class will happily let you create malformed rectangles (that is,
-// rectangles with negative width and/or height), but there will be assertions
-// in the operations (such as Contains()) to complain in this case.
+/*
+ * Copyright (C) 2023 Amlogic, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef RECT_H_
 #define RECT_H_
